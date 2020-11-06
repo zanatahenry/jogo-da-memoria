7cf1fb1e95ed0620e6e3f322e68827502aa4522b
Henry Zanata <henryzanata@hotmail.com>
2020-11-06 12:14:52 -0300

    firt commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const cards = document.querySelectorAll('.card')
+let hasFlippedCard = false
+let firstCard, secondCard
+let lockBoard = false
+
+function flipCard(){
+    if(lockBoard) return
+
+    this.classList.add('flip')
+
+    if(!hasFlippedCard){
+        hasFlippedCard = true
+        firstCard = this
+        return
+    }
+
+    secondCard = this
+    hasFlippedCard = false
+    checkForMath()
+}
+
+function checkForMath(){
+    if(firstCard.dataset.card === secondCard.dataset.card){
+        disableCards()
+        return
+    }
+
+    unflipCards()
+}
+
+function disableCards(){
+    firstCard.removeEventListener('click', flipCard)
+    secondCard.removeEventListener('click', flipCard)
+}
+
+function unflipCards(){
+    setTimeout(() => {
+        firstCard.classList.remove('flip')
+        secondCard.classList.remove('flip')
+    }, 700)
+}
+
+cards.forEach((card) =>{
+    card.addEventListener('click', flipCard)
+})
