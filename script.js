40d003514bd1fe5dc5defd8a9b66812c8e4d5449
Henry Zanata <henryzanata@hotmail.com>
2020-11-10 01:25:43 -0300

    final

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let lockBoard = false
 
 function flipCard(){
     if(lockBoard) return
+    if(this === firstCard) return
 
     this.classList.add('flip')
 
@@ -31,14 +32,32 @@ function checkForMath(){
 function disableCards(){
     firstCard.removeEventListener('click', flipCard)
     secondCard.removeEventListener('click', flipCard)
+
+    resetBoard()
 }
 
 function unflipCards(){
+    lockBoard = true
+
     setTimeout(() => {
         firstCard.classList.remove('flip')
         secondCard.classList.remove('flip')
+
+        resetBoard()
     }, 700)
 }
+
+function resetBoard(){
+    [hasFlippedCard, lockBoard] = [false, false]
+    [firstCard, secondCard] = [null, null]
+}
+
+(function shuffle(){
+    cards.forEach((card) =>{
+        let randomPosition = Math.floor(Math.random() * 12)
+        card.style.order = randomPosition
+    })
+}) ()
 
 cards.forEach((card) =>{
     card.addEventListener('click', flipCard)
